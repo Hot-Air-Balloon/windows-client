7ed63321402c54f56b4a73ea08966bfc0b713254
mpr0xy <mpr0xy@live.com>
2020-07-17 21:02:02 +0430

    local proxy is ok

--- WindowsFormsApp1/SocketListener.cs
+++ WindowsFormsApp1/SocketListener.cs
@@ -21,6 +21,16 @@ namespace WindowsFormsApp1
         private Dictionary<int, List<Thread>> recvThreadList = new Dictionary<int, List<Thread>>();
         private Form1 mainForm;
 
+        /**
+         * 把16进制字符串传为字节数组
+         */
+        private static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
         public static SocketListener Instance()
         {
             if (listenerIns == null)
@@ -72,54 +82,45 @@ namespace WindowsFormsApp1
                 }
             }
         }
-        private static byte[] StringToByteArray(string hex)
+        /**
+         * 返回客户端需要连接的目标地址和IP
+         */
+        private Dictionary<string, Object> GetRemoteInfo(Socket clientSock)
         {
-            return Enumerable.Range(0, hex.Length)
-                             .Where(x => x % 2 == 0)
-                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
-                             .ToArray();
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="clientSocket"></param>
-        private void HandleSocket(object clientSocket)
-        {
-            ArrayList arrayList = clientSocket as ArrayList;
-            int index = (int)arrayList[0];
-            Socket clientSock = arrayList[1] as Socket;
-            String targetAddr = "";
+            Dictionary<string, Object> targetInfo = new Dictionary<string, Object>();
+            targetInfo.Add("addr", "");
+            targetInfo.Add("port", 0);
+            string targetAddr = "";
             short targetPort = 0;
+            // 验证连接是否是socket格式
             try
             {
-                // if (clientSock.Poll(-1, SelectMode.SelectRead) && clientSock.Available == 0)
-                // {
-                //    clientSock.Close(); ;
-                // }
                 byte[] strbyte = new byte[256];
                 int count = clientSock.Receive(strbyte);
-                mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端第一次发送的数据量{1}", index, count));
+                mainForm.SetLogTextBox(string.Format("代号为:的客户端第一次发送的数据量{0}", count));
                 if (count == 0)
                 {
-                    mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端初始化数据错误", index));
-                    clientSock.Close(); ;
+                    mainForm.SetLogTextBox(string.Format("的客户端初始化数据错误"));
+                    return targetInfo;
                 }
                 clientSock.Send(StringToByteArray("0500"));
                 byte[] addrType = new byte[4];
                 clientSock.Receive(addrType);
-                mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端初始化数据{1} {2}", index, addrType[0], addrType[1]));
+                mainForm.SetLogTextBox(string.Format("代号为:的客户端初始化数据{0} {1}", addrType[0], addrType[1]));
                 if (Convert.ToInt32(addrType[1]) != 1)
                 {
-                    mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端connect错误", index));
-                    clientSock.Close(); ;
+                    mainForm.SetLogTextBox(string.Format("代号为:的客户端connect错误"));
+                    return targetInfo;
                 }
                 IPAddress ipa = null;
+                // 判断socks5的3种类型
                 switch (addrType[3])
                 {
                     case 1:
                         byte[] addrIP = new byte[4];
                         clientSock.Receive(addrIP);
                         ipa = new IPAddress(IPAddress.NetworkToHostOrder(BitConverter.ToInt32(addrIP, 0)));
+                        targetAddr = ipa.ToString();
                         break;
                     case 3:
                         byte[] addrLen = new byte[1];
@@ -133,95 +134,96 @@ namespace WindowsFormsApp1
                         byte[] ipv6AddrLen = new byte[16];
                         clientSock.Receive(ipv6AddrLen);
                         ipa = new IPAddress(IPAddress.NetworkToHostOrder(BitConverter.ToInt64(ipv6AddrLen, 0)));
+                        targetAddr = ipa.ToString();
                         break;
                 }
                 byte[] targetPortByte = new byte[2];
                 clientSock.Receive(targetPortByte);
                 Array.Reverse(targetPortByte);
                 targetPort = BitConverter.ToInt16(targetPortByte, 0);
-                if (ipa != null)
-                {
-                    mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端connect ip {1}", index, ipa.ToString()));
-                }
-                if (count > 0)
-                {
-                    string ret = Encoding.UTF8.GetString(strbyte, 0, count);
-                    mainForm.SetLogTextBox(string.Format("{0}给你发送了消息：{1}", clientSock.RemoteEndPoint, ret));
-                    // clientSock.Send(Encoding.UTF8.GetBytes("test"));
-                }
+                // 告诉客户端可以开始传输数据
+                byte[] sendData = new byte[10];
+                SocketListener.StringToByteArray("05000001").CopyTo(sendData, 0);
+                IPAddress.Parse("127.0.0.1").GetAddressBytes().CopyTo(sendData, 4);
+                short sendPort = 1080;
+                BitConverter.GetBytes(IPAddress.HostToNetworkOrder(sendPort)).CopyTo(sendData, 8);
+                clientSock.Send(sendData);
             } catch (Exception)
             {
-                mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端异常离去！", index));
-                recvThreadList[index][0].Abort();
+                return targetInfo;
             }
-            byte[] sendData = new byte[11];
-            SocketListener.StringToByteArray("05000001").CopyTo(sendData, 0);
-            IPAddress.Parse("127.0.0.1").GetAddressBytes().CopyTo(sendData, 4);
-            short sendPort = 1080;
-            BitConverter.GetBytes(IPAddress.HostToNetworkOrder(sendPort)).CopyTo(sendData, 8);
-            mainForm.SetLogTextBox(BitConverter.ToString(sendData));
-            clientSock.Send(sendData);
+            targetInfo["addr"] = targetAddr;
+            targetInfo["port"] = targetPort;
+            return targetInfo;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="clientSocket"></param>
+        private void HandleSocket(object clientSocket)
+        {
+            ArrayList arrayList = clientSocket as ArrayList;
+            int index = (int)arrayList[0];
+            Socket clientSock = arrayList[1] as Socket;
+            Dictionary<string, Object> remoteInfo = GetRemoteInfo(clientSock);
+            if ((string)(remoteInfo["addr"]) == String.Empty)
+            {
+                clientSock.Close();
+                recvThreadList[index][0].Abort();
+                return;
+            }
+            // 给目标建立连接
+
             Socket targetSock = new Socket(AddressFamily.InterNetwork,
                     SocketType.Stream,
                     ProtocolType.Tcp);
+            Thread sendTargetThread = null;
             try
             {
-                targetSock.Connect(targetAddr, targetPort);
-            } catch(Exception)
-            {
-                mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端 目标连接异常", index));
-                clientSock.Close();
-                recvThreadList[index][0].Abort();
+                targetSock.Connect((string)remoteInfo["addr"], (short)remoteInfo["port"]);
+                sendTargetThread = new Thread(ThreadForTarget);
+                sendTargetThread.Start(new ArrayList { clientSock, targetSock });
+                LoopReceiveAndSend(targetSock, clientSock);
+            } catch (Exception) {
+                mainForm.SetLogTextBox(string.Format("客户端发送接受异常"));
             }
-            while (true)
-            {
-                try
-                {
-                    if (clientSock.Poll(1, SelectMode.SelectRead) && clientSock.Available == 0)
-                    {
-                        break;
-                    }
-                    if (targetSock.Poll(1, SelectMode.SelectRead) && targetSock.Available == 0)
-                    {
-                        break;
-                    }
-                    if (clientSock.Poll(1, SelectMode.SelectRead))
-                    {
-                        byte[] data = new byte[4096 * 10];
-                        int length = clientSock.Receive(data);
-                        if (length == 0)
-                        {
-                            mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端 收到数据是0", index));
-                            break;
-                        }
-                        int sendLength = targetSock.Send(data, length, SocketFlags.None);
-                        mainForm.SetLogTextBox(System.Text.Encoding.ASCII.GetString(data));
-                        if (length != sendLength)
-                        {
-                            break;
-                        }
-                    }
-                    if (targetSock.Poll(1, SelectMode.SelectRead))
-                    {
-                        byte[] _data = new byte[4096 * 10];
-                        int tlength = targetSock.Receive(_data);
-                        int sendLength = clientSock.Send(_data, tlength, SocketFlags.None);
-                        mainForm.SetLogTextBox(System.Text.Encoding.ASCII.GetString(_data));
-                        if (tlength != sendLength)
-                        {
-                            mainForm.SetLogTextBox("数据未发送完");
-                            break;
-                        }
-                    }
-                } catch (Exception)
-                {
-                    mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端异常离去！", index));
-                    recvThreadList[index][0].Abort();
-                }
-            }
-            mainForm.SetLogTextBox(string.Format("代号为:{0}的客户端正常离去！", index));
+            mainForm.SetLogTextBox(string.Format("终止客户端连接"));
             clientSock.Close();
             targetSock.Close();
+            recvThreadList[index][0].Abort();
+            sendTargetThread.Abort();
+            return;
+        }
+        private void ThreadForTarget(object socketList)
+        {
+            ArrayList arrayList = socketList as ArrayList;
+            Socket clientSock = arrayList[0] as Socket;
+            Socket targetSock = arrayList[1] as Socket;
+            try
+            {
+                LoopReceiveAndSend(clientSock, targetSock);
+            } catch(Exception)
+            {
+                mainForm.SetLogTextBox("发送给目标数据异常");
+            }
+        }
+        private void LoopReceiveAndSend(Socket reviceSock, Socket sendSock)
+        {
+            while(true)
+            {
+                if (reviceSock.Poll(1, SelectMode.SelectRead) && reviceSock.Available == 0)
+                {
+                    break;
+                }
+                byte[] data = new byte[4096 * 10];
+                int tlength = reviceSock.Receive(data);
+                int sendLength = sendSock.Send(data, 0, tlength, SocketFlags.None);
+                // mainForm.SetLogTextBox(System.Text.Encoding.ASCII.GetString(_data));
+                if (tlength != sendLength)
+                {
+                    break;
+                }
+            }
         }
     }
 }
